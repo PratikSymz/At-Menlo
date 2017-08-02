66c6eae678cef0a7097b5d5891545faa0389f5fa
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2017-08-02 18:47:43 +0530

    Changed Package Name from "example" to "pratiksymz"

--- app/src/main/java/com/pratiksymz/android/menlolookaround/PlacePagerAdapter.java
+++ app/src/main/java/com/pratiksymz/android/menlolookaround/PlacePagerAdapter.java
@@ -6,8 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import com.example.android.menlolookaround.R;
-
 public class PlacePagerAdapter extends FragmentPagerAdapter {
 
     private Context mContext;
